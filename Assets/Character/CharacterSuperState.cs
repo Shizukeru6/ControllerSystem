91203a394a987fe33f338bab789ef778a4513200
Shizukeru6 <Shizukeru6@users.noreply.github.com>
2023-07-04 01:11:12 +0300

    Input System

--- Assets/Character/CharacterSuperState.cs
+++ Assets/Character/CharacterSuperState.cs
@@ -4,14 +4,29 @@ using UnityEngine;
 
 public class CharacterSuperState : MonoBehaviour
 {
+    [field : SerializeField] public Animator animator { get; private set; }
+    private string Speed;
 
     public void Walk()
     {
-
+        Speed = "Walk";
+        SetLayer();
     }
 
     public void RunSlow()
     {
+        Speed = "RunSlow";
+        SetLayer();
+    }
 
+    public void Run()
+    {
+        Speed = "Run";
+        SetLayer();
+    }
+
+    private void SetLayer()
+    {
+        
     }
 }
